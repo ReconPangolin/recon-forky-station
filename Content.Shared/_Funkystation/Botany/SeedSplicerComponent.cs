3fa90df58306862affc478b459108e9a7204344d
ReconPangolin <reconpangolin@gmail.com>
2026-07-23 23:39:12 +1000

    Feat: Added basic seed splicer component, system and yml

---
+++ Content.Shared/_Funkystation/Botany/SeedSplicerComponent.cs
@@ -0,0 +1,26 @@
+﻿using Robust.Shared.GameStates;
+using Content.Shared.Containers.ItemSlots;
+
+namespace Content.Shared._Funkystation.Botany;
+
+/// <summary>
+/// This is used for...
+/// </summary>
+[RegisterComponent, NetworkedComponent]
+public sealed partial class SeedSplicerComponent : Component
+{
+
+
+    public const string SeedSlotLeftId = "SeedSplicer-left";
+    public const string SeedSlotRightId = "SeedSplicer-right";
+    public const string ResourceSlotId = "SeedSplicer-resource";
+
+    [DataField]
+    public ItemSlot SeedSlotLeft = new();
+
+    [DataField]
+    public ItemSlot SeedSlotRight = new();
+
+    [DataField]
+    public ItemSlot ResourceSlot = new();
+}
---
+++ Content.Shared/_Funkystation/Botany/SharedSeedSplicerSystem.cs
@@ -0,0 +1,46 @@
+﻿using Content.Shared.Access.Components;
+using Content.Shared.Access.Systems;
+using Content.Shared.Containers.ItemSlots;
+using Robust.Shared.Containers;
+
+namespace Content.Shared._Funkystation.Botany;
+
+/// <summary>
+/// This handles...
+/// </summary>
+public sealed partial class SharedSeedSplicerSystem : EntitySystem
+{
+    [Dependency] private ItemSlotsSystem ItemSlotsSystem = default!;
+
+    /// <inheritdoc/>
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<SeedSplicerComponent, ComponentInit>(OnComponentInit);
+        SubscribeLocalEvent<SeedSplicerComponent, ComponentRemove>(OnComponentRemove);
+
+        //SubscribeLocalEvent<SeedSplicerComponent, EntInsertedIntoContainerMessage>(OnItemInserted);
+        //SubscribeLocalEvent<SeedSplicerComponent, EntRemovedFromContainerMessage>(OnItemRemoved);
+
+    }
+
+    private void OnComponentInit(EntityUid uid, SeedSplicerComponent splicer, ComponentInit args)
+    {
+        ItemSlotsSystem.AddItemSlot(uid, SeedSplicerComponent.ResourceSlotId, splicer.ResourceSlot);
+        ItemSlotsSystem.AddItemSlot(uid, SeedSplicerComponent.SeedSlotLeftId, splicer.SeedSlotLeft);
+        ItemSlotsSystem.AddItemSlot(uid, SeedSplicerComponent.SeedSlotRightId, splicer.SeedSlotRight);
+
+    }
+
+    private void OnComponentRemove(EntityUid uid, SeedSplicerComponent splicer, ComponentRemove args)
+    {
+        ItemSlotsSystem.RemoveItemSlot(uid, splicer.ResourceSlot);
+        ItemSlotsSystem.RemoveItemSlot(uid, splicer.SeedSlotLeft);
+        ItemSlotsSystem.RemoveItemSlot(uid, splicer.SeedSlotRight);
+    }
+
+
+
+
+}
